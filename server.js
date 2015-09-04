e9b749d69e8d7ceacf31a700e3365b731dba16cc
Connor McKelvey <connormckelvey@gmail.com>
2015-09-04 15:56:19 -0600

    chnaged path for server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
-var express = require('express')l
+var path = require('path');
+var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/_site'));
+app.use(express.static(path.join(__dirname,'_site')));
 app.listen(process.env.PORT || 5000);
